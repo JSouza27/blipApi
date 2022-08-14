7973a5a428a3d400c833f3a14771778672221fc1
Jonathan Souza <souzajonathan570@gmail.com>
2022-08-14 16:07:40 -0300

    feat: adiciona documentação

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const swaggerUi = require('swagger-ui-express');
 const app = require('./src/config/express')();
 const router = require('./src/routes/gitHubRoutes');
+const swaggerDocument = require('./swagger.json');
 
 const port = app.get('port');
 
@@ -7,10 +9,11 @@ app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
 
-app.get('/', (_req, res) => {
+app.get('/api', (_req, res) => {
   res.send({ status: 'ok' });
 });
 
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', router);
 
 module.exports = app;
