a03007d02ee20d07611b7dab80d310ce773cde73
Jonathan Souza <souzajonathan570@gmail.com>
2022-08-14 15:00:01 -0300

    feat: adiciona testes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('./src/config/express')();
+const router = require('./src/routes/gitHubRoutes');
 
 const port = app.get('port');
 
@@ -9,3 +10,7 @@ app.listen(port, () => {
 app.get('/', (_req, res) => {
   res.send({ status: 'ok' });
 });
+
+app.use('/api', router);
+
+module.exports = app;
---
+++ src/test/integration.spec.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiHttp = require('chai-http');
+const server = require('../../index.js');
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Testanto integração com api do GitHub', () => {
+  describe('GET /avatar', () => {
+    let response;
+
+    before(async () => {
+      response = await chai.request(server).get('/api/avatar');
+    })
+
+    it('Deve retornar status 200', () => {
+      expect(response).to.have.status(200);
+    })
+
+    it('Deve retornar um objeto no body', () => {
+      expect(response.body).to.have.a('object');
+    })
+
+    it('Deve retornar a propriedade "avatar" ', () => {
+      expect(response.body).to.have.property('avatar');
+    })
+  });
+
+  describe('GET /api/repo', () => {
+    let response;
+
+    before(async () => {
+      response = await chai.request(server).get('/api/repo');
+    })
+
+    it('Deve retornar status 200', () => {
+      expect(response).to.have.status(200);
+    })
+
+    it('Deve retornar um Array no body', () => {
+      expect(response.body).to.have.a('array');
+    })
+
+    it('Deve retornar a propriedade "title" e " subtitle', () => {
+      expect(response.body[0]).to.have.property('title');
+      expect(response.body[0]).to.have.property('subtitle');
+    })
+  });
+});
